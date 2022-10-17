b1e2d6519b31bd9aaef9e2ba43c282b20c05325a
Jorge Nicolau <4923759+jlnicolau@users.noreply.github.com>
2022-10-16 23:09:28 -0700

    Inclusion of the get and post handles. Finished

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
+//first require the packages
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
+//start the express object.
+const app = express();
+//use bodyParser as a third-part middleware
+//(http://expressjs.com/en/guide/using-middleware.html#middleware.third-party)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//it controls the get response-request cycle.
+//when the client reaches the port 3000 (defined by the app.listen() method below) the access send a GET request to the router "/". For that case, the response will be the index.html file. 
 app.get("/", function (req, res) {
-   res.send("hello !!!")
+   //res.send("eita !!!")
+   res.sendFile(__dirname + "/index.html");
 });
+
+//it controls the post response-request cycle.
+app.post("/", function (req, res) {
+
+   let num1 = Number(req.body.num1);
+   let num2 = Number(req.body.num2);
+
+   let result = num1 + num2;
+   res.send("Thanks for posting that. The result is: " + result);
+})
+
+//start listen to the port for requests
 app.listen('3000');
